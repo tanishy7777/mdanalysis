2548cfe53a6547de641de6c7f29bd2f8d5ea01e9
Tanish Yelgoe <143334319+tanishy7777@users.noreply.github.com>
2025-02-04 19:35:17 +0530

    Update package/MDAnalysis/analysis/polymer.py
    Co-authored-by: Rocco Meli <r.meli@bluemail.ch>

--- package/MDAnalysis/analysis/polymer.py
+++ package/MDAnalysis/analysis/polymer.py
@@ -124,11 +124,10 @@ class PersistenceLength(AnalysisBase):
        C(n) = \langle \cos\theta_{i, i+n} \rangle =
                \langle \mathbf{a_i} \cdot \mathbf{a_{i+n}} \rangle
                
-    where :math:`\mathbf{a}_i \text{ and } \mathbf{a}_{i+n}` are unit vectors
-    along the bonds. (Because for two vectors a and b, 
-    :math:`\mathbf{a} \cdot \mathbf{b} = |\mathbf{a}| |\mathbf{b}| \cos{\alpha}`)
-    – see 
-    `Wikipedia <https://en.wikipedia.org/wiki/Dot_product#Geometric_definition>`_.
+    where :math:`a_i` and :math:`a_{i+n}` are unit vectors
+    along the bonds. (Because for two vectors :math:`\mathbf{a}` and :math:`\mathbf{b}`, 
+    :math:`\mathbf{a} \cdot \mathbf{b} = |\mathbf{a}| |\mathbf{b}| \cos{\alpha}`,
+    where :math:`\alpha` is the angle between the two vectors.)
 
     An exponential decay is then fitted to this, which yields the
     persistence length
